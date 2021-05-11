4c7deaa1ec78a043177112dd8586115745e4c653
chandu velagapudi <chandu.velagapudi22@gmail.com>
2021-05-11 12:20:43 +0530

    This is my frist commit for HyderabadDeloite Code

---
+++ src/com/info/tech/deloitr/HydDeloite.java
@@ -0,0 +1,20 @@
+package com.info.tech.deloitr;
+
+public class HydDeloite {
+	public static void ui_Jobs() {
+		System.out.println("Welcome to Deloite UI JOBS");
+
+	}
+
+	public void testing_Jobs() {
+		System.out.println("Welcome to Deloite Testing  JOBS");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to deloite");
+		HydDeloite deloite = new HydDeloite();
+		ui_Jobs();
+		deloite.testing_Jobs();
+
+	}
+}
